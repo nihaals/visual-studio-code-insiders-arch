da0b58eca80322c7a5a9a12687a3e73a106641db
Nihaal Sangha <18350092+nihaals@users.noreply.github.com>
2020-08-12 22:38:51 +0100

    Changed update-version to Python script

---
+++ .github/actions/update-version/update-version.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+
+with open('PKGBUILD') as fp:
+    for line in fp.readlines():
+        line = line.strip()
+        current_version = re.search(r"^pkgver=(.+)$", line)
+        if current_version is None:
+            continue
+        current_version = current_version.group(1)
+        break
+    else:
+        raise ValueError("pkgver not found")
+
+latest_version = os.environ['VERSION']
+latest_hash_x86_64 = os.environ['SHA256_x86_64']
+
+print(f'Current pkgver: {current_version}')
+print(f'Latest pkgver: {latest_version}')
+
+with open('PKGBUILD') as fp:
+    contents = fp.read()
+
+if current_version != latest_version:
+    contents = re.sub(r"^pkgrel=.+$", 'pkgrel=1', contents, flags=re.MULTILINE)
+
+contents = re.sub(r"^pkgver=.+$", f'pkgver={latest_version}', contents, flags=re.MULTILINE)
+contents = re.sub(r"(sha256sums_x86_64=\(\n  ').+'\n", f"\g<1>{latest_hash_x86_64}'\n", contents)
+
+with open('PKGBUILD', 'w') as fp:
+    fp.write(contents)
