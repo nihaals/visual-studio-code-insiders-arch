9593f7a14fef4e57a23b11cbb4798b864ff4e9af
Nihaal Sangha <nihaal.git@gmail.com>
2021-02-07 00:38:53 +0000

    Add more logs to pkg action

--- .github/actions/pkg/entrypoint.sh
+++ .github/actions/pkg/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Setting permissions..."
 chmod -R a+rw .
 
+echo "Writing SRCINFO..."
 sudo -Eu builder makepkg --printsrcinfo > .SRCINFO
 
+echo "Getting package file..."
 PKGFILE="$(sudo -Eu builder makepkg --packagelist)"
-echo "Package: $PKGFILE"
+echo "Package file: $PKGFILE"
 
 # Build packages
+echo "Building package..."
 sudo -EH -u builder makepkg --syncdeps --noconfirm
 
 # makepkg reports absolute paths, must be relative for use by other actions
