677e3155ebc0a86f8f2da78358d1bd1ee910649f
Nihaal Sangha <nihaal.git@gmail.com>
2021-03-07 14:57:34 +0000

    Format generate-index

--- .github/actions/generate-index/generate-index.sh
+++ .github/actions/generate-index/generate-index.sh
@@ -6,8 +6,8 @@ fi
 
 for DIR in $(find "$1" -path "$1"/.git -prune -o -type d); do
   {
-      echo -e '<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>'
-      ls -1pa -I.git "$DIR" | grep -v "^\./$" | grep -v "^index\.html$"  | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-      echo -e '</pre>\n</body>\n</html>'
+    echo -e '<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>'
+    ls -1pa -I.git "$DIR" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    echo -e '</pre>\n</body>\n</html>'
   } > "$DIR"/index.html
 done
