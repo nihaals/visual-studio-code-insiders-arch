ce5c1a34210f7644ce6fa85fd864ade16b61a75a
Nihaal Sangha <nihaal.git@gmail.com>
2024-01-05 02:04:40 +0000

    Support comments in flags file

--- visual-studio-code-insiders.sh
+++ visual-studio-code-insiders.sh
@@ -4,7 +4,7 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Support setting global flags
 if [[ -f "${XDG_CONFIG_HOME}/code-flags.conf" ]]; then
-   CODE_USER_FLAGS="$(cat "${XDG_CONFIG_HOME}/code-flags.conf")"
+    CODE_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/code-flags.conf)"
 fi
 
 exec /opt/visual-studio-code-insiders/bin/code-insiders $CODE_USER_FLAGS "$@"
