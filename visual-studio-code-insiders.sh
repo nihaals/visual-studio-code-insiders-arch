a34771e52882d112b3a86665268e10f904e9939d
Nihaal Sangha <nihaal.git@gmail.com>
2024-01-05 02:04:09 +0000

    Update comments

--- visual-studio-code-insiders.sh
+++ visual-studio-code-insiders.sh
@@ -2,11 +2,10 @@
 
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
-# Allow users to override command-line options
+# Support setting global flags
 if [[ -f "${XDG_CONFIG_HOME}/code-flags.conf" ]]; then
    CODE_USER_FLAGS="$(cat "${XDG_CONFIG_HOME}/code-flags.conf")"
 fi
 
-# Launch
 exec /opt/visual-studio-code-insiders/bin/code-insiders $CODE_USER_FLAGS "$@"
 
